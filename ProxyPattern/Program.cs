795a3555418647ca42ddfbce0d84ed803eab41ef
SeyedsaeidHosseini <SeyedsaeidHosseini1990@gmail.com>
2024-07-25 22:46:50 +0430

    Add project files.

---
+++ ProxyPattern/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Isubject subject = new proxy();
+
+subject.DoAction();
+
+public interface Isubject
+{
+    void DoAction();
+}
+
+public class SubjectService : Isubject
+{
+    public void DoAction()
+    {
+        Console.WriteLine("main Method");
+    }
+}
+public class proxy : Isubject
+{
+    private Isubject _subject;
+    public void DoAction()
+    {
+        if (_subject == null)
+        {
+            _subject = new SubjectService();
+        }
+        Console.WriteLine("proxy method");
+    }
+}
